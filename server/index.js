8e35e9b9ffee3e923cd8ad7de56d0183df480088
Naim Buru <nbur4u@gmail.com>
2017-06-17 21:56:03 +0300

    React and webpack are installed and configured.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import path from 'path';
 
+import webpack from 'webpack';
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackConfig from '../webpack.config.dev';
+
 let app = express();
 
-app.get('/*', (req, res) =>{
-  res.send(path.join(__dirname, './index.html'));
+app.use(webpackMiddleware(webpack(webpackConfig)));
+
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, './index.html'));
 });
 
 app.listen(3000, () => console.log('Listening from localhost:3000'));
