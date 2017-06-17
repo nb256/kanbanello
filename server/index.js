af7610625c8ce0d9420fb444e600c0a613a957fb
Naim Buru <nbur4u@gmail.com>
2017-06-17 13:04:23 +0300

    .jshintrc file added.

--- server/index.js
+++ server/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import path from 'path';
 
 let app = express();
 
 app.get('/*', (req, res) =>{
-  res.send('hello')
+  res.send(path.join(__dirname, './index.html'));
 });
 
-app.listen(3000, () => console.log('s'))
+app.listen(3000, () => console.log('Listening from localhost:3000'));
